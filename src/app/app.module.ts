23da663d25c69fc08fc97490b67414bdfc7cadfc
furkanogutcu <56116207+furkanogutcu@users.noreply.github.com>
2021-09-04 01:02:55 +0300

    Add Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrandComponent } from './components/brand/brand.component';
+import { ColorComponent } from './components/color/color.component';
+import { CustomerComponent } from './components/customer/customer.component';
+import { CarComponent } from './components/car/car.component';
+import { RentalComponent } from './components/rental/rental.component';
+import { NaviComponent } from './components/navi/navi.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BrandComponent,
+    ColorComponent,
+    CustomerComponent,
+    CarComponent,
+    RentalComponent,
+    NaviComponent
   ],
   imports: [
     BrowserModule,
